66323fe5b7352767ad0ea21c2e9d39cce04fd85f
lloukmane <lukatoni1992@gmail.com>
2021-12-12 16:02:10 +0000

    terraform template

---
+++ lab2/TodoServerless/Ui/vars.js
@@ -0,0 +1,9 @@
+// If noApi is set to true, the API will not be called
+var noApi = false;
+
+// If noLogin is set to true, the app will not try to log in
+var noLogin = true;
+
+// Put the urls to your local api and your remote API here
+var localUrl = "";
+var remoteUrl = "";
